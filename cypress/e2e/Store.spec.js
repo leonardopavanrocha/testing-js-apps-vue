9551fa826e7cbeaba9b87710f35a04133bed9ee6
Leonardo Pavan Rocha <leonardo.rocha.pavan@gmail.com>
2023-06-18 17:57:19 +0200

    add: cypress config

---
+++ cypress/e2e/Store.spec.js
@@ -0,0 +1,7 @@
+describe('Store', () => {
+  it('should display the store', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('body').contains('Brand');
+    cy.get('body').contains('Wrist Watch');
+  });
+});
